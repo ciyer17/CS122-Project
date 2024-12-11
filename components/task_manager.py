7cbb7a942204c25c9d831864d49a08640402588a
gr33npo1s0n <iyerchandramouli03@gmail.com>
2024-12-11 04:42:31 -0800

    Completed Task Addition and Fetching. Also Error Handling

--- components/task_manager.py
+++ components/task_manager.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import os
 from tkinter import ttk
+from tkinter import messagebox
 import sqlite3
 from datetime import datetime
 
@@ -11,6 +12,7 @@ class TaskManager:
         self.conn = None
         self.cursor = None
         self.root['background'] = '#ff9f2a'
+        self.root.minsize(width=1280, height=950)
 
         self.open_db_conn()
 
@@ -35,6 +37,9 @@ class TaskManager:
     def return_to_home(self):
         from main import SmartClockApp
         '''Return to the home screen'''
+        self.cursor.execute('''
+            DROP TABLE IF EXISTS tags;
+        ''')
         self.close_db_conn()
         SmartClockApp(self.root)        
 
@@ -52,9 +57,10 @@ class TaskManager:
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title TEXT NOT NULL,
                 due_date TEXT NOT NULL,
-                due_time TEXT NOT NULL,
+                due_time TEXT,
                 description TEXT,
                 tag_id INTEGER,
+                status TEXT CHECK(status IN ('completed', 'upcoming', 'overdue')) NOT NULL,
                 FOREIGN KEY (tag_id) REFERENCES tags (id)
             )
         ''')
@@ -63,36 +69,43 @@ class TaskManager:
         self.cursor.execute("INSERT OR IGNORE INTO tags (name) VALUES ('misc')")
         self.conn.commit() # Commit changes to the database
 
+    def clear_window(self):
+        '''Clear all widgets in the window'''
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
     def create_task_manager_ui(self):
         '''Create the task manager UI'''
-        
-        image_dir = os.path.join(os.getcwd(), "images")
-        home_img = tk.PhotoImage(file=f"{image_dir}/home.png")
-        home_button = ttk.Button(self.root, image=home_img, command=self.return_to_home)
-        home_button.place(x=0, y=0, anchor="nw")
+
+        self.clear_window()
 
         bg_style = ttk.Style()
         bg_style.configure('Custom.TFrame', background='#ff9f2a')
 
+        btn_style = ttk.Style()
+        btn_style.configure('Custom.TButton', background='#ffef0a', relief='solid', font=('Arial', 18), width=15)
+
+        home_button = ttk.Button(self.root, text='Home', style='Custom.TButton', command=self.return_to_home).pack(anchor=tk.NW, padx=30, pady=30)
+
         # Split UI into left and right frames
         self.left_frame = ttk.Frame(self.root, style='Custom.TFrame')
-        self.left_frame.pack(side="left", fill="y", padx=10, pady=10)
+        self.left_frame.pack(side="left", fill="y", padx=30, pady=30)
 
         self.right_frame = ttk.Frame(self.root, style='Custom.TFrame')
         self.right_frame.pack(side="right", expand=True, fill="both", padx=10, pady=10)
 
         # New task interface
-        btn_style = ttk.Style()
-        btn_style.configure('Custom.TButton', background='#ffef0a', relief='solid', font=('Arial', 18), width=15)
-
         self.new_task_button = ttk.Button(self.left_frame, style='Custom.TButton', text="Create New Task", command=self.render_task_form)
         self.new_task_button.pack(pady=20)
 
         # Task list on the right
-        self.task_list_label = ttk.Label(self.right_frame, text="Tasks", font=("Arial", 16))
+        task_frame = ttk.Frame(self.right_frame, style='Custom.TFrame')
+        task_frame.pack(expand=True, fill='both')
+
+        self.task_list_label = ttk.Label(task_frame, background='#ff9f2a', text="Tasks", font=("Arial", 16))
         self.task_list_label.pack(pady=10)
 
-        self.task_list = tk.Listbox(self.right_frame, height=20, width=50)
+        self.task_list = tk.Listbox(task_frame, height=20, width=50)
         self.task_list.pack(pady=10)
 
         # Load tasks into the list
@@ -104,37 +117,104 @@ class TaskManager:
         for widget in self.left_frame.winfo_children():
             widget.destroy()
 
-        ttk.Label(self.left_frame, text="New Task", font=("Arial", 16)).pack(pady=10)
+        bg_style = ttk.Style()
+        bg_style.configure('Custom.TFrame', background='#ff9f2a')
+
+        task_frame = ttk.Frame(self.left_frame, style='Custom.TFrame')
+        task_frame.pack(expand=True, fill='both')
+
+        ttk.Label(task_frame, anchor='center', background='#ff9f2a', text="New Task", font=("Arial", 16)).pack(pady=10)
 
         # Task fields
-        self.title_entry = self.create_form_field("Title:")
-        self.date_entry = self.create_form_field("Date (YYYY-MM-DD):")
-        self.time_entry = self.create_form_field("Time (HH:MM AM/PM or leave blank for all-day):")
-        self.description_entry = self.create_form_field("Description:", True)
+        self.title_entry = self.create_form_field(task_frame, "Title:")
+        date_vars = self.create_date_incrementer(task_frame)
+        self.date_entry = f"{date_vars[0].get()}-{date_vars[1].get()}-{date_vars[2].get()}"
+        time_vars = self.create_time_selector(task_frame)
+        self.time_entry = (time_vars[0], time_vars[1])  # hour_var, minute_var
+        self.am_pm_var = time_vars[2]  # am_pm_var
+        self.description_entry = self.create_form_field(task_frame, "Description:", True)
 
-        # Tag dropdown
-        ttk.Label(self.left_frame, text="Tag:").pack(anchor="w", pady=5)
+        # Tag management
+        ttk.Label(task_frame, background='#ff9f2a', text="Tag:").pack(anchor="w", pady=5)
         self.tag_var = tk.StringVar()
-        self.tag_dropdown = ttk.Combobox(self.left_frame, textvariable=self.tag_var, state="readonly")
+        self.tag_entry = ttk.Entry(task_frame, textvariable=self.tag_var)
+        self.tag_entry.pack(fill="x", pady=5)
+        ttk.Button(task_frame, text="Add Tag", command=self.add_new_tag).pack(pady=5)
+        self.tag_dropdown = ttk.Combobox(task_frame, state="readonly")
         self.tag_dropdown.pack(fill="x", pady=5)
         self.load_tags()
 
-        # Save button
-        ttk.Button(self.left_frame, text="Save Task", command=self.save_task).pack(pady=10)
+        button_frame = ttk.Frame(task_frame, style='Custom.TFrame', )
+        button_frame.pack(pady=100, anchor=tk.CENTER, expand=True, fill='both')
 
-        # Back button
-        ttk.Button(self.left_frame, text="Back", command=self.create_task_manager_ui).pack(pady=10)
+        btn_style = ttk.Style()
+        btn_style.configure('Custom.TButton', background='#ffef0a', relief='solid', font=('Arial', 18), width=15)
 
-    def create_form_field(self, label_text, is_multiline=False):
+        # Task Options
+        ttk.Button(button_frame, style='Custom.TButton', text="Save Task", command=lambda: self.save_task(date_vars[2], date_vars[0], date_vars[1])).pack(side=tk.LEFT, padx=20)
+        ttk.Button(button_frame, style='Custom.TButton', text="Back", command=lambda: self.create_task_manager_ui).pack(side=tk.LEFT, padx=20)
+
+    def create_form_field(self, frame, label_text, is_multiline=False):
         '''Create a form field with a label'''
-        ttk.Label(self.left_frame, text=label_text).pack(anchor="w", pady=5)
+        ttk.Label(frame, anchor='center', background='#ff9f2a', text=label_text).pack(anchor="w", pady=5)
         if is_multiline:
-            entry = tk.Text(self.left_frame, height=4)
+            entry = tk.Text(frame, height=4)
         else:
-            entry = ttk.Entry(self.left_frame)
+            entry = ttk.Entry(frame)
         entry.pack(fill="x", pady=5)
         return entry
     
+    def create_date_incrementer(self, frame):
+        ttk.Label(frame, anchor='center', background='#ff9f2a', text="Date (MM-DD-YYYY):").pack(anchor="w", pady=5)
+
+        bg_style = ttk.Style()
+        bg_style.configure('Custom.TFrame', background='#ff9f2a')
+
+        date_frame = ttk.Frame(frame, style='Custom.TFrame')
+        date_frame.pack(fill="x", pady=5)
+
+        month_var = tk.StringVar(value=str(datetime.now().month))
+        month_spinbox = ttk.Spinbox(date_frame, from_=1, to=12, width=3, textvariable=month_var, validate='key')
+        month_spinbox.configure(validatecommand=(self.root.register(self.validate_month), '%P'))
+        month_spinbox.grid(row=0, column=0, padx=5)
+
+        date_var = tk.StringVar(value=str(datetime.now().day))
+        date_spinbox = ttk.Spinbox(date_frame, from_=1, to=31, width=3, textvariable=date_var, validate='key')
+        date_spinbox.configure(validatecommand=(self.root.register(lambda value: self.validate_day(value, year_var, month_var)), '%P'))
+        date_spinbox.grid(row=0, column=1, padx=5)
+
+        year_var = tk.StringVar(value=str(datetime.now().year))
+        year_spinbox = ttk.Spinbox(date_frame, from_=2024, to=2040, width=6, textvariable=year_var, validate='key')
+        year_spinbox.configure(validatecommand=(self.root.register(self.validate_year), '%P'))
+        year_spinbox.grid(row=0, column=2, padx=5)
+
+        return (month_var, date_var, year_var)
+
+    def create_time_selector(self, frame):
+        ttk.Label(frame, anchor='center', background='#ff9f2a', text="Time (Leave Empty for All-Day):").pack(anchor="w", pady=5)
+
+        bg_style = ttk.Style()
+        bg_style.configure('Custom.TFrame', background='#ff9f2a')
+
+        time_frame = ttk.Frame(frame, style='Custom.TFrame')
+        time_frame.pack(fill="x", pady=5)
+
+        hour_var = tk.StringVar()
+        hour_spinbox = ttk.Spinbox(time_frame, background='#ff9f2a', from_=1, to=12, width=3, textvariable=hour_var, validate='key')
+        hour_spinbox.configure(validatecommand=(self.root.register(self.validate_hour), '%P'))
+        hour_spinbox.grid(row=0, column=0, padx=5)
+
+        minute_var = tk.StringVar()
+        minute_spinbox = ttk.Spinbox(time_frame, background='#ff9f2a', from_=0, to=59, width=3, textvariable=minute_var, format="%02.0f", validate='key')
+        minute_spinbox.configure(validatecommand=(self.root.register(self.validate_minutes), '%P'))
+        minute_spinbox.grid(row=0, column=1, padx=5)
+
+        am_pm_var = tk.StringVar(value="AM")
+        am_pm_dropdown = ttk.Combobox(time_frame, background='#ff9f2a', textvariable=am_pm_var, values=["AM", "PM"], state="readonly", width=5)
+        am_pm_dropdown.grid(row=0, column=2, padx=5)
+
+        return (hour_var, minute_var, am_pm_var)
+
     def convert_time_to_24hour(self, time_str):
         '''Converts a time string from 12-hour format to 24-hour format'''
         try:
@@ -148,13 +228,62 @@ class TaskManager:
             return datetime.strptime(time_str, "%H:%M").strftime("%I:%M %p")
         except ValueError:
             return None
+        
+    def validate_month(self, value):
+        if value.isdigit():
+            month = int(value)
+            return month >= 1 and month <= 12
+        return False
+    
+    def validate_day(self, value, year_var, month_var):
+        if value.isdigit():
+            day = int(value)
+            month = int(month_var.get())
+            year = int(year_var.get())
+            days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+            if month == 2 and year % 4 == 0:
+                days_in_month[1] = 29
+
+            return 1 <= day <= days_in_month[month - 1]
+        return False
+        
+    def validate_year(self, value):
+        if value.isdigit():
+            year = int(value)
+            return year >= 2024 and year <= 2040
+        return False
+    
+    def validate_hour(self, value):
+        if value.isdigit():
+            hour = int(value)
+            return hour >= 1 and hour <= 12
+        return False
+    
+    def validate_minutes(self, value):
+        if value.isdigit():
+            minute = int(value)
+            return minute >= 0 and minute <= 59
+        return False
 
     def load_tags(self):
         '''Load tags from the database and populate the dropdown'''
         self.cursor.execute("SELECT name FROM tags")
         tags = [tag[0] for tag in self.cursor.fetchall()]
         self.tag_dropdown["values"] = tags
-        self.tag_var.set("misc")
+        if not self.tag_var.get():
+            self.tag_var.set("misc")
+
+
+    def add_new_tag(self):
+        new_tag = self.tag_var.get().strip()
+        if new_tag:
+            self.cursor.execute("INSERT OR IGNORE INTO tags (name) VALUES (?)", (new_tag,))
+            self.conn.commit()
+            self.load_tags()
+        else:
+            tk.messagebox.showerror("Error", "Tag name cannot be empty.")
+
 
     def load_tasks(self):
         '''Load tasks from the database and display them in the listbox'''
@@ -163,7 +292,7 @@ class TaskManager:
 
         # Fetch and display tasks
         self.cursor.execute('''
-            SELECT tasks.title, tasks.due_date, tasks.due_time, tasks.description, tags.name
+            SELECT tasks.title, tasks.due_date, tasks.due_time, tasks.description, tags.name, tasks.status
             FROM tasks
             LEFT JOIN tags ON tasks.tag_id = tags.id
         ''')
@@ -171,36 +300,47 @@ class TaskManager:
 
         for task in tasks:
             task_time = self.convert_time_to_ampm(task[2]) if task[2] else 'All-Day'
-            task_display = f"{task[0]} | {task[1]} {task_time} | {task[4]}"
+            task_display = f"{task[0]} | {task[1]} {task_time} | {task[4]} | {task[5]}"
             self.task_list.insert(tk.END, task_display)
 
-    def save_task(self):
+    def save_task(self, year_var, month_var, day_var):
         '''Save a new task to the database'''
         # Save new task to the database
         title = self.title_entry.get()
-        due_date = self.date_entry.get()
-        due_time = self.convert_time_to_24hour(self.time_entry.get().strip()) or None
-
-        # Select the entire text from the textbox from line 1 column 0 to the end
-        description = self.description_entry.get("1.0", tk.END).strip()
-        tag = self.tag_var.get()
-
-        if not title or not due_date:
-            tk.messagebox.showerror("Error", "Title and Date are required fields.")
+        if title == "":
+            messagebox.showerror("Error", "Title cannot be empty.")
             return
+        
+        due_date = f"{year_var.get()}-{int(month_var.get()):02}-{int(day_var.get()):02}"
+        hour = self.time_entry[0].get()
+        minute = self.time_entry[1].get()
+        am_pm = self.am_pm_var.get()
+        due_time = self.convert_time_to_24hour(f"{hour}:{minute} {am_pm}") if hour and minute else None
+        description = self.description_entry.get("1.0", tk.END).strip()
 
-        # Get tag ID, or create if it doesn't exist
-        self.cursor.execute("INSERT OR IGNORE INTO tags (name) VALUES (?)", (tag,))
+        # Handle tag
+        tag_name = self.tag_var.get().strip()
+        if not tag_name:
+            tag_name = "misc"
+        self.cursor.execute("INSERT OR IGNORE INTO tags (name) VALUES (?)", (tag_name,))
         self.conn.commit()
-        self.cursor.execute("SELECT id FROM tags WHERE name = ?", (tag,))
-
-        # Fetches the tag id from the database. Returned as a tuple, so we get the first element
+        self.cursor.execute("SELECT id FROM tags WHERE name = ?", (tag_name,))
         tag_id = self.cursor.fetchone()[0]
+
+        # Determine task status based on date and time
+        task_datetime_str = f"{due_date} {due_time}" if due_time else due_date
+        task_datetime = datetime.strptime(task_datetime_str, "%Y-%m-%d %H:%M") if due_time else datetime.strptime(task_datetime_str, "%Y-%m-%d")
+        current_datetime = datetime.now()
+
+        if task_datetime < current_datetime:
+            status = "overdue"
+        else:
+            status = "upcoming"
 
         # Insert task into the database
         self.cursor.execute(
-            "INSERT INTO tasks (title, due_date, due_time, description, tag_id) VALUES (?, ?, ?, ?, ?)",
-            (title, due_date, due_time, description, tag_id)
+            "INSERT INTO tasks (title, due_date, due_time, description, tag_id, status) VALUES (?, ?, ?, ?, ?, ?)",
+            (title, due_date, due_time, description, tag_id, status)
         )
         self.conn.commit()
 
